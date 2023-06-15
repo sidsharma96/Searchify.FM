6858d042aeea16eb9b39efebf4b13817f19cf579
Siddharth Sharma <siddharthsharma@Siddharths-MacBook-Pro.local>
2023-06-14 23:58:48 -0700

    Hiding API Key and changing styling

--- src/util/ApiUrlHandler.js
+++ src/util/ApiUrlHandler.js
@@ -1,7 +1,5 @@
-import { ApiUtil } from './ApiUtil.js';
-
 export const ApiUrlHandler = (param) => {
-  var ApiKey = ApiUtil.apiKey;
+  var ApiKey = process.env.REACT_APP_API_KEY;
   var getInfoUrl = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${param}&api_key=${ApiKey}&format=json`;
   var getTopAlbumsUrl = `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${param}&api_key=${ApiKey}&format=json`;
   return {
--- src/util/ApiUtil.js
+++
@@ -1,4 +0,0 @@
-export const ApiUtil = {
-  apiKey: '8b5d3db871e34b646ff34825fcd61d39',
-  apiSecret: '9f61a8d769ed035ff0be07be8d6a69af',
-};
